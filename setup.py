131c7ba5914d815410692ad85a72a5cb75b82fac
Killian Mahé <killianmahe.pro@gmail.com>
2021-09-24 09:25:28 -0400

    🔧 Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='Hoover Agent',
+      version='0.1',
+      description='UQAC IA project',
+      author='Killian Mahé',
+      author_email='killian.mahe1@uqac.ca',
+      packages=[
+          'pretty-tables',
+          'colorama'
+      ]
+)
